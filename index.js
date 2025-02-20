d1e1af4401cd38809e3aac5825fd14916dd8a49f
Raihan <aboraihan14@gmail.com>
2025-02-21 05:37:55 +0600

    patch method and get by email

--- index.js
+++ index.js
@@ -55,7 +55,26 @@ async function run() {
       res.send(result);
     });
 
-   
+    // PUT API to Update a Task
+    app.patch("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const taskData = req.body;
+      const updatedDoc = {
+        $set: { ...taskData },
+      };
+      const result = await tasksCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+    app.get("/task/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await tasksCollection.find(query).toArray();
+      res.send(result);
+    });
+
+
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
